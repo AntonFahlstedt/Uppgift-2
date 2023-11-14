bd0b88c628bb8f98639bb45ea649a2bcd6c29e31
Anton Fahlstedt <antonfahlstedt@live.com>
2023-11-14 20:42:58 +0100

    Initial Commit

---
+++ Uppgift 2/Uppgift_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift_2App.swift
+//  Uppgift 2
+//
+//  Created by Anton Fahlstedt on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
