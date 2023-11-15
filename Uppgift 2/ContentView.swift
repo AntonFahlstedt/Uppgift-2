8aa5f295d622991deb4ff666fbab577e6d1b3c43
Anton Fahlstedt <antonfahlstedt@live.com>
2023-11-15 13:54:13 +0100

    Koden för uppgift 2

--- Uppgift 2/ContentView.swift
+++ Uppgift 2/ContentView.swift
@@ -8,16 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    //Variabel för inmatning i textruta
+    @State var Groc = ""
+    
+    //Tom lista för matvaror
+    @State var groc_list = [String]()
+    
     var body: some View {
+        
+    
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack {
+                
+                TextField("Produkt", text: $Groc)
+                    .font(.system(size: 32, weight: .bold, design: .rounded))
+                    .padding(.all)
+
+
+    
+                Button(action: {
+                    groc_list.append(Groc)
+                    Groc = ""
+                }){
+                    Text("Lägg till")
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                        .frame(width: 150, height: 50)
+                        .background(
+                            Color.blue
+                                .cornerRadius(20))
+                    
+                .padding(.all)
+                }
+            
+            }
+        
+
+
+        
+            
+                List(groc_list, id: \.self) {produkt in
+                    Text(produkt)
+                        .padding(.all)
+                    
+                
+                    
+                }
+                
+        
+            }
+            Button(action: {
+                groc_list.removeAll()
+            }){
+                Text("Nollställ")
+                    .font(.system(size: 18, weight: .bold, design: .rounded))
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 50)
+                    .background(
+                        Color.red
+                            .cornerRadius(20))
+                    .padding(.all)
+            }
+
         }
-        .padding()
+
     }
-}
 
 #Preview {
     ContentView()
